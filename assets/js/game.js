845266001ad4b34f46181884def142cc2785c896
Shubhanshu Mishra <shubhanshumishra@gmail.com>
2014-08-06 14:40:46 -0500

    New Features: 1. New game button. 2. Line to show matching cells.
    Fixed Errors:
    1. End game condition especially for draw game during last chance.
    2. Multiple selection of same cell when already selected.
    3. Clearing of wrong cell from last turn.
    
    Minor Fixes:
    - Added motivation to page.
    - Added author detail on game.js
    - Fixed canvas size issue.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,11 @@
+/**
+	Probabilistic Tic Tac Toe game.
+	Author: Shubhanshu Mishra (shubhanshumishra@gmail.com)
+	Initial code for drawing the tic tac toe on canvas borrowed from online forums. 
+	Toss version implemented by the author.
+**/
+
+
 var xBoard = 0;
 var oBoard = 0;
 var begin = true;
@@ -10,13 +18,23 @@ var playPositions = [[-1, -1], [-1, -1]];
 var currPos = 0;
 var currPlayer = -1;
 var selectedPositions = 0;
+var totalMoves = 0;
 
 function paintBoard() {
 	var board = document.getElementById('board');
-
-	width = board.width;
-	height = board.height;
+	
+	board.style.width='70%';
+	board.style.height='70%';
+	var minDim = board.offsetHeight < board.offsetWidth ? board.offsetHeight: board.offsetWidth ;
+	board.width  = minDim;
+	board.height = minDim;
+	
 	context = board.getContext('2d');
+	//context.style.width = '100%';
+	//context.style.height = '100%';
+	
+	width = context.canvas.width;
+	height = context.canvas.height;
 
 	context.beginPath();
 	context.strokeStyle = '#000';
@@ -117,6 +135,44 @@ function paintO(x, y, strokeColor) {
 	context.closePath();
 }
 
+function paintEnd(board, strokeColor) {
+	
+	var bX, bY, eX, eY;
+	console.log("Winning Board: %s, %s", board, board.toString(2));
+	if(((board | 0x1C0) == board) ) {bX = 0; bY = 2.5; eX = 3; eY = 2.5; }
+	if(((board | 0x38) == board) ) {bX = 0; bY = 1.5; eX = 3; eY = 1.5; }
+	if(((board | 0x7) == board) ) {bX = 0; bY = 0.5; eX = 3; eY = 0.5; }
+	if(((board | 0x124) == board) ) {bX = 2.5; bY = 0; eX = 2.5; eY = 3; }
+	if(((board | 0x92) == board) ) {bX = 1.5; bY = 0; eX = 1.5; eY = 3; }	
+	if(((board | 0x49) == board) ) {bX = 0.5; bY = 0; eX = 0.5; eY = 3; }
+	if(((board | 0x111) == board)) {bX = 0; bY = 0; eX = 3; eY = 3; }
+	if(((board | 0x54) == board) ) {bX = 3; bY = 0; eX = 0; eY = 3; }
+	
+	console.log("Winning Board: %s, %s", board, board.toString(2));
+	console.log("Winning Line Points bX: %s, bY: %s, eX: %s, eY: %s", bX, bY, eX, eY);
+	
+	
+	context.beginPath();
+	if (typeof strokeColor === 'undefined'){
+		strokeColor = '#00A521';
+		}
+	context.strokeStyle = strokeColor;
+	context.lineWidth = 4;
+
+	var beginX = bX * (width / 3) ;
+	var beginY = bY * (height / 3);
+
+	var endX = eX * (width / 3);
+	var endY = eY * (height / 3);
+
+	context.moveTo(beginX, beginY);
+	context.lineTo(endX, endY);
+
+	context.stroke();
+	context.closePath();
+}
+
+
 var playerPaintFuncts = [paintO, paintX];
 
 function clearPlayerPosition(posX, posY){
@@ -147,8 +203,16 @@ function clickHandler(e) {
 		return false;
 	}
 
-	console.log("ClientX: %s, Width/3: %s, X: %s, MouseX: %s", e.clientX, width / 3, x, mouseX);
-	console.log("ClientY: %s, Height/3: %s, Y: %s, MouseY: %s", e.clientY, height / 3, y, mouseY);
+	//console.log("ClientX: %s, Width/3: %s, X: %s, MouseX: %s", e.clientX, width / 3, x, mouseX);
+	//console.log("ClientY: %s, Height/3: %s, Y: %s, MouseY: %s", e.clientY, height / 3, y, mouseY);
+	
+	
+	
+	if(((playPositions[0][0] == x && playPositions[0][1] == y) ||
+		(playPositions[1][0] == x && playPositions[1][1] == y)) && totalMoves < 8){
+		alert("Please choose a different cell.");
+		return false;
+	}
 	
 	var lastPosition = [playPositions[currPos][0], playPositions[currPos][1]];
 	
@@ -168,10 +232,11 @@ function clickHandler(e) {
 }
 
 function checkNobody() {
-	if ((xBoard | oBoard) == 0x1FF) {
+	console.log("Total Moves: %s", totalMoves);
+	if ((xBoard | oBoard) == 0x1FF && totalMoves < 9) {
 		//alert('Nobody won!!');
 		showResult(-1);
-		restart();
+		//restart();
 		return true;
 	}
 	return false;
@@ -185,6 +250,7 @@ function restart() {
 	currPos = 0;
 	selectedPositions = 0;
 	playPositions = [[-1, -1], [-1, -1]]
+	totalMoves = 0;
 	$("#playerPos > tbody:last > tr").remove();
 	//$('#playerPos > tbody:last').append('<tr><td>Player '+currPlayer+'</td><td></td><td></td><td></td><td></td></tr>');
 	paintBoard();
@@ -193,79 +259,6 @@ function restart() {
 
 function isEmpty(xBoard, oBoard, bit) {
 	return (((xBoard & bit) == 0) && ((oBoard & bit) == 0));
-}
-
-function simulate(oBoard, xBoard) {
-
-	var ratio = 0;
-
-	var bit = 0;
-	for (var i = 0; i < 9; i++) {
-
-		var cBit = 1 << i;
-
-		if (isEmpty(xBoard, oBoard, cBit)) {
-
-			if (checkWinner(oBoard | cBit)) {
-				bit = cBit;
-				break;
-			} else if (checkWinner(xBoard | cBit)) {
-				bit = cBit;
-			}
-		}
-	}
-
-	if (bit == 0) {
-		for (var i = 0; i < 9; i++) {
-			var cBit = 1 << i;
-
-			if (isEmpty(xBoard, oBoard, cBit)) {
-				var result = think(oBoard, xBoard, 'X', 0, 1)
-					if (ratio == 0 || ratio < result) {
-						ratio = result;
-						bit = cBit;
-					}
-			}
-		}
-	}
-	return bit;
-}
-
-function think(oBoard, xBoard, player, bit, ratio) {
-
-	if (player == 'O') {
-		oBoard = oBoard | bit;
-	} else {
-		xBoard = xBoard | bit;
-	}
-
-	if (checkWinner(oBoard)) {
-		ratio *= 1.1;
-		return ratio;
-
-	} else if (checkWinner(xBoard)) {
-
-		ratio *= 0.7;
-		return ratio;
-
-	} else {
-		var best = 0;
-		ratio *= 0.6;
-
-		for (var i = 0; i < 9; i++) {
-
-			if (isEmpty(xBoard, oBoard, 1 << i)) {
-
-				var newRatio = think(oBoard, xBoard, player == 'O' ? 'X' : 'O', 1 << i, ratio);
-
-				if (best == 0 || best < newRatio) {
-					best = newRatio;
-				}
-			}
-		}
-
-		return best;
-	}
 }
 
 function markBit(markBit, player) {
@@ -291,16 +284,10 @@ function markBit(markBit, player) {
 	}
 }
 
-function play() {
-	var bestBit = simulate(oBoard, xBoard);
-
-	markBit(bestBit, 'O');
-
-}
-
 function showResult(player) {
 	if(player === 1 || player === 0){
 		$("#result").text("Congratulations player " + player + ", you won.");
+		paintEnd(player === 0 ? oBoard : xBoard);
 	}
 	else{
 		$("#result").text("Nobody won.");
@@ -314,13 +301,15 @@ function nextPlayer(){
 	 $('#playerPos > tbody:last').append('<tr><td>Player '+currPlayer+'</td><td></td><td></td><td></td><td></td><td></td></tr>');
 	 $('#current-player').text("Player "+currPlayer+"'s turn.");
 	selectedPositions = 0;
-	playPositions = [[-1, -1], [-1, -1]]
+	playPositions = [[-1, -1], [-1, -1]];
+	totalMoves += 1;
+	currPos = 0;
 	}
 
 var playerIcons = ['O', 'X'];
 function toss() {
 	if (selectedPositions < 2){
-		alert("Please select atleast 2 positions and then click toss.");
+		alert("Please select at least 2 positions and then click toss.");
 		return false;
 		}
 
@@ -351,27 +340,23 @@ function toss() {
 		clearPlayerPosition(playPositions[1][0], playPositions[1][1]);
 
 		markBit(bit, playerIcons[currPlayer]);
-
+		
 		if (!checkNobody()) {
-			if (checkWinner(xBoard)) {
+			var currBoard = currPlayer == 0 ? oBoard : xBoard;
+			if (checkWinner(currBoard)) {
 
 				//alert('You win!!');
 				showResult(currPlayer);
-				restart();
+				//restart();
 
-			} else {
-				//Uncomment below to play with AI.
-				// play();
-				if (!checkNobody()) {
-					if (checkWinner(oBoard)) {
-						//alert('Loser!!');
-						showResult(currPlayer);
-						restart();
-					}
-					else{
-						nextPlayer();
-						}
-				}
+			}
+			else if (totalMoves == 9){
+				showResult(-1);
+			}
+			else {
+
+				nextPlayer();
+
 			}
 		}
 	}
